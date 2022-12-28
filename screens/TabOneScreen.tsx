092bbfe21211b3f6b649953f049c1ad38f3f00c9
abhishek <abbhishekk27@gmail.com>
2022-12-29 02:06:38 +0530

    form

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,32 +1,63 @@
 import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+// import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
+import { View, Input, FormControl, Select, Flex, Button } from 'native-base';
+import { useState } from 'react';
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-    </View>
-  );
-}
+	const [workout, setWorkout] = useState<string>('');
+	const [day, setDay] = useState<string>('');
+	const [sets, setSets] = useState<string>();
+	const [reps, setReps] = useState<string>();
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
+	const handleChangeWorkout = (text: string) => {
+		setWorkout(text);
+	};
+	const handleChangeSets = (text: string) => {
+		setWorkout(text);
+	};
+	const handleChangeReps = (text: string) => {
+		setWorkout(text);
+	};
+
+	return (
+		<View p='8' bg='#000' minH='full'>
+			<FormControl>
+				<FormControl.Label>Workout</FormControl.Label>
+				<Input value={workout} mb='2' onChangeText={handleChangeWorkout} />
+				<FormControl.Label>Day</FormControl.Label>
+				<Select
+					accessibilityLabel='Day'
+					placeholder='Day'
+					mb='2'
+					selectedValue={day}
+					onValueChange={(itemValue) => setDay(itemValue)}>
+					<Select.Item label='PUSH' value='push' />
+					<Select.Item label='PULL' value='pull' />
+					<Select.Item label='LEGS' value='legs' />
+				</Select>
+				<Flex direction='row'>
+					<Flex>
+						<FormControl.Label>Sets</FormControl.Label>
+						<Input
+							mb='2'
+							w='48px'
+							mr='8'
+							value={sets}
+							onChangeText={handleChangeSets}
+						/>
+					</Flex>
+					<Flex>
+						<FormControl.Label>Reps</FormControl.Label>
+						<Input mb='2' w='48px' value={reps} onChangeText={handleChangeReps} />
+					</Flex>
+				</Flex>
+			</FormControl>
+			<Button variant='outline' color='#fff' _text={{ color: '#fff' }}>
+				Add Workout
+			</Button>
+		</View>
+	);
+}
