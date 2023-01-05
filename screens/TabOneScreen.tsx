e7939d1b12e48d6b8ed51ad6e23df41a19f98372
abhishek <abbhishekk27@gmail.com>
2023-01-06 00:30:48 +0530

    filter by day

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,7 +1,3 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '../components/EditScreenInfo';
-// import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 import {
 	View,
@@ -14,12 +10,15 @@ import {
 	Icon,
 	FlatList,
 	Text,
+	Modal,
+	Heading,
 } from 'native-base';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import AntDesign from '@expo/vector-icons/AntDesign';
 import { WorkoutRealmContext } from '../models';
 import { Workout } from '../models/Workout';
+
 // add workout
 
 // - name
@@ -29,14 +28,102 @@ import { Workout } from '../models/Workout';
 // if no of reps ===
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+	const [isModalVisible, setModalVisible] = useState<boolean>(false);
+	const [workoutSplit, setWorkoutSplit] = useState<string | null>(null);
+
+	const checkModal = async () => {
+		try {
+			const lastDisplayed = await AsyncStorage.getItem('lastDisplayed');
+			if (!lastDisplayed) {
+				setModalVisible(true);
+				// await AsyncStorage.setItem('lastDisplayed', 'true');
+			} else {
+				const elapsedTime = Date.now() - parseInt(lastDisplayed);
+				if (elapsedTime >= 8 * 60 * 60 * 1000) {
+					setModalVisible(true);
+					// await AsyncStorage.setItem('lastDisplayed', 'true');
+				}
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	useEffect(() => {
+		checkModal();
+	}, []);
+
+	const handleSelectWorkoutSplit = async (type: string) => {
+		setWorkoutSplit(type);
+		setModalVisible(false);
+		// close the modal
+		// try {
+		// 	await AsyncStorage.setItem('lastDisplayed', Date.now().toString());
+		// } catch (error) {
+		// 	console.log(error);
+		// }
+	};
+
 	const { useQuery } = WorkoutRealmContext;
 	const workouts = useQuery(Workout);
-	console.log(workouts);
+	const day = workouts.filtered(`day == '${workoutSplit}'`);
+	console.log(workouts, workoutSplit);
 	return (
 		<>
+			<Modal
+				isOpen={isModalVisible}
+				onClose={() => setModalVisible(false)}
+				size='full'
+				h='100%'
+				flex={1}
+				bgColor='#000'>
+				<Modal.Content bgColor='#000'>
+					<Heading
+						color='#fff'
+						textAlign='center'
+						fontSize='3xl'
+						mb='24px'
+						fontWeight={300}>
+						Select Workout Day
+					</Heading>
+					<Button
+						w='75%'
+						mx='auto'
+						my='12px'
+						py='20px'
+						variant='outline'
+						borderRadius='lg'
+						_text={{ color: '#fff', fontSize: 'xl', fontWeight: 300 }}
+						onPress={() => handleSelectWorkoutSplit('push')}>
+						Push
+					</Button>
+					<Button
+						w='75%'
+						mx='auto'
+						my='12px'
+						py='20px'
+						borderRadius='lg'
+						variant='outline'
+						_text={{ color: '#fff', fontSize: 'xl', fontWeight: 300 }}
+						onPress={() => handleSelectWorkoutSplit('pull')}>
+						Pull
+					</Button>
+					<Button
+						w='75%'
+						py='20px'
+						mx='auto'
+						my='12px'
+						borderRadius='lg'
+						variant='outline'
+						_text={{ color: '#fff', fontSize: 'xl', fontWeight: 300 }}
+						onPress={() => handleSelectWorkoutSplit('legs')}>
+						Legs
+					</Button>
+				</Modal.Content>
+			</Modal>
 			<FlatList
-				data={workouts}
-				keyExtractor={(task) => task._id.toString()}
+				data={day}
+				keyExtractor={(day) => day._id.toString()}
 				renderItem={({ item }) => <Text color='#fff'>{item.name}</Text>}
 			/>
 			<Fab
